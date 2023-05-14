69d715638adacdfdfc0eb3cf59af184e095309dc
kikegm <kikegarcia58@gmail.com>
2023-05-14 11:27:50 +1000

    Significant refactoring and enhancement of the TelegramBot class and audio processing functionalities. Key updates include improved readability, better error handling, optimized audio download and conversion mechanisms, and reduction of dependencies
    -Refactored TelegramBot class for better readability and separation of concerns.
    -Simplified retry logic in send_files_via_telegram().
    -Added retry logic for 'streamingData' errors in process_message().
    -Initialized bot and processor in constructor using environment variables.
    -Replaced pytube and moviepy libraries with yt_dlp for YouTube audio download, improving reliability and reducing dependencies.
    -Implemented hashing for file naming to prevent file name collision.
    -Enhanced the audio splitting functionality for longer audio, creating sub-audios of equal duration.
    -Added support for webm format which is commonly used by YouTube.
    -Removed the use of ffmpeg python library, replaced it with a subprocess call for better control and error handling.
    -Optimized the 'clean_title' method by removing the re.sub() redundancy.
    -Implemented 'AudioProcessor' as a class instead of a collection of functions, encapsulating related functionalities and improving code readability.
    -Streamlined the 'download_audio' method to handle YouTube and Twitch URLs uniformly.
    -Improved error handling across the class by adding more detailed exception messages.

--- audio_processing.py
+++ audio_processing.py
@@ -3,17 +3,14 @@ import os
 import re
 import subprocess
 import unicodedata
-
-import ffmpeg
-from moviepy.video.io.VideoFileClip import AudioFileClip
-from pytube import YouTube
+import yt_dlp
 import requests
 from bs4 import BeautifulSoup
 
 
 def shorten_title(title, max_length=50):
     """
-    Shortens the title of a file if it's too long
+    Truncate the title of a file if it's too long.
     """
     if len(title) <= max_length:
         return title
@@ -23,118 +20,146 @@ def shorten_title(title, max_length=50):
     return f"{short_title}_{hex_dig}"
 
 
+
+
 class AudioProcessor:
+    # Desired duration of a sub_audio clip in seconds
+    sub_audio_duration = 30 * 60
+
     def __init__(self):
         pass
 
     def download_audio(self, url):
         """
-        Downloads the audio of the given video URL
+        Download audio from the video provided by the URL.
         """
         if "youtube.com" in url:
             return self.download_audio_youtube(url)
         elif "twitch.tv" in url:
-            return self.download_audio_twitch(url)
+            return self.download_video_twitch(url)
         else:
             raise Exception("Sorry, this script only supports downloading audio from YouTube and Twitch.")
 
     def download_audio_youtube(self, url):
         """
-        Downloads the audio from YouTube of the given video URL
+        Download YouTube audio from the video provided by the URL.
         """
-        yt = YouTube(url)
-        stream = yt.streams.get_audio_only()
-        stream.download()
-        mp4_file = stream.get_file_path()
+        song_title = clip = None
+        try:
+            # First, fetch the video's info
+            with yt_dlp.YoutubeDL({}) as ydl:
+                info_dict = ydl.extract_info(url, download=False)
+                song_title = self.clean_title(re.sub(r'[^\w\s-]', '', info_dict['title']).strip().replace(' ', '_'))
+                song_title = shorten_title(song_title)
 
-        song_title = self.clean_title(re.sub(r'[^\w\s-]', '', yt.title).strip().replace(' ', '_'))
-        song_title = shorten_title(song_title)
+            # Then, download the audio and convert it to mp3 format
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'outtmpl': f'{song_title}.webm',  # output filename includes the cleaned video's title
+            }
 
-        clip = AudioFileClip(mp4_file)
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
 
-        return song_title, clip, mp4_file
+            webm_file = f"{song_title}.webm"  # webm file named after the cleaned video's title
+            return song_title, webm_file
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
-    def convert_audio(self, song_title, clip, mp4_file):
+    def createSubAudios(self, duration, song_title, webm_file):
         """
-        Converts the downloaded audio file to mp3 format
+        Splits the audio into multiple sub-audios based on specified duration.
+        """
+        num_sub_audios = int(duration / self.sub_audio_duration) + 1
+        mp3_files = []
+        # Create a folder to store sub-audios
+        sub_audio_folder = os.path.join(os.getcwd(), f"{song_title}")
+        os.makedirs(sub_audio_folder, exist_ok=True)
+        for i in range(num_sub_audios):
+            start_time = i * self.sub_audio_duration
+            end_time = (i + 1) * self.sub_audio_duration
+            if end_time > duration:
+                end_time = duration
+            mp3_file = os.path.join(sub_audio_folder, f"{song_title}_{i + 1}.mp3")
+            self.convert_webm_to_mp3(webm_file, mp3_file, start_time, end_time)
+            mp3_files.append(mp3_file)
+        # Remove the webm file after creating all sub_audios
+        self.remove_files(webm_file)
+        return mp3_files
+
+    def convert_webm_to_mp3(self, webm_file, mp3_file, start_time, end_time):
+        """
+        Converts the webm file to mp3 using ffmpeg
+        """
+        try:
+            subprocess.run([
+                'ffmpeg', '-y', '-i', webm_file, '-ss', str(start_time), '-to', str(end_time),
+                '-vn', '-ar', '44100', '-ac', '2', '-b:a', '192k', '-threads', '8', mp3_file
+            ], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error in converting {webm_file} to {mp3_file}: {e}")
+
+    def get_audio_duration(self, audio_file):
+        """
+        Get the duration of an audio file in seconds
+        """
+        try:
+            result = subprocess.run(['ffprobe', '-v', 'error', '-show_entries',
+                                     'format=duration', '-of',
+                                     'default=noprint_wrappers=1:nokey=1', audio_file],
+                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            output = result.stdout.strip()
+            if not output:
+                raise ValueError("Empty output received from ffprobe")
+
+            duration = float(output)
+            return duration
+        except subprocess.CalledProcessError as e:
+            print(f"Error in getting duration of {audio_file}: {e}")
+            return None
+        except ValueError as e:
+            print(f"Error in converting ffprobe output to float: {e}")
+            return None
+
+    def convert_audio(self, song_title, clip):
+        """
+        Convert the downloaded audio file to mp3 format
         """
         song_file_path = f"{song_title}.mp3"
         clip.write_audiofile(song_file_path)
 
-    def create_sub_audios(self, song_title, clip):
-        """
-        Splits the downloaded audio file into sub-audios
-        """
-        sub_audios_dir = song_title
-        os.makedirs(sub_audios_dir, exist_ok=True)
-
-        duration = clip.duration
-        max_duration = 30 * 60
-        num_sub_audios = int(duration / max_duration) + 1
-        sub_audio_duration = int(duration / num_sub_audios)
-
-        for i in range(num_sub_audios):
-            start_time = i * sub_audio_duration
-            end_time = (i + 1) * sub_audio_duration
-            if end_time > duration:
-                end_time = duration
-            sub_audio = clip.subclip(start_time, end_time)
-            sub_audio_file_path = os.path.join(sub_audios_dir, f"{song_title}_{i + 1}.mp3")
-            sub_audio.write_audiofile(os.path.abspath(sub_audio_file_path))
-
-        clip.close()
-        print("Audio has been downloaded and split in sub-audios.")
-
     def remove_audios(self, song_title, mp4_file):
         """
-        Deletes the downloaded audio files
+        Remove the downloaded audio files
         """
         mp3_file = f"{song_title}.mp3"
         self.remove_files(mp4_file, mp3_file)
 
     def remove_files(self, *file_paths):
         """
-        Deletes a list of files
+        Remove a list of files
         """
         for file_path in file_paths:
             if os.path.exists(file_path):
                 os.remove(file_path)
-    def convert_video_to_audio(self, song_title, mp4_file):
-        """
-        Converts the downloaded video file to mp3 format
-        """
-        song_file_path = f"{song_title}.mp3"
-        (
-            ffmpeg
-            .input(mp4_file)
-            .output(song_file_path)
-            .run()
-        )
 
-    def download_audio_twitch(self, url):
+    def download_video_twitch(self, url):
         """
-        Downloads the audio from Twitch of the given video URL
+        Download the Twitch video provided by the URL
         """
         title = self.scrap_twitch_video_title(url)
-        vod_path = f"./{title}.mp4"
+        vod_path = f"./{title}.webm"
         if not os.path.exists(vod_path):
-            subprocess.call(["streamlink", url, "audio", "-o", vod_path, "--stream-segment-threads=8", "--hls-live-edge=6",
-                         "--stream-segment-timeout=5",])
+            subprocess.call(
+                ["streamlink", url, "audio", "-o", vod_path, "--stream-segment-threads=8", "--hls-live-edge=6",
+                 "--stream-segment-timeout=5", ])
 
-        mp3_path = f"./{title}.mp3"
-        if not os.path.exists(mp3_path):
-            self.convert_video_to_audio(title, vod_path)
-
-        print("Audio has been downloaded.")
-        print("Video file has been deleted.")
-
-        clip = AudioFileClip(mp3_path)
-
-        return title, clip, vod_path
+        print("Video has been downloaded.")
+        return title, vod_path
 
     def scrap_twitch_video_title(self, url):
         """
-        Gets the title of the Twitch video from the webpage
+        Retrieve the title of the Twitch video from the web page
         """
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -148,10 +173,27 @@ class AudioProcessor:
 
     def clean_title(self, title):
         """
-        Cleans the title of special characters
+        Clean the title of special characters
         """
         title = unicodedata.normalize('NFKD', title).encode('ASCII', 'ignore').decode('utf-8')
         title = re.sub(r'[^\w\s\-]+', '', title).strip().replace(' ', '_')
         title = shorten_title(title)
 
         return title
+
+if __name__ == '__main__':
+        while True:
+            # Prompt the user to enter a YouTube URL
+            url = input("Enter a YouTube video URL to download the audio: ")
+
+            # Create a new instance of the AudioProcessor class
+            audio_processor = AudioProcessor()
+
+            try:
+                # Download the audio from the URL and create sub-audios
+                song_title, *webm_file = audio_processor.download_audio(url)
+
+                print("Audio has been downloaded and split into sub-audios.")
+            except Exception as e:
+                # If an exception occurs, send error message to console
+                print(f"An error occurred: {e}")
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,65 +1,61 @@
-# Import necessary libraries
 import os
+
 import telebot
 from audio_processing import AudioProcessor
 from tqdm import tqdm
 from time import sleep
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
 
 class TelegramBot:
-    def __init__(self):
-        # Initialize the Telegram bot and AudioProcessor objects
-        self.bot = telebot.TeleBot(os.environ.get("TELEGRAM_BOT_TOKEN"))
+    """
+    This class handles the operations of a Telegram bot that processes and sends audio files.
+    """
+    def __init__(self, token):
+        """
+        Initializes the Telegram bot with the given token, an instance of the AudioProcessor,
+        and sets up the progress bar emojis and message.
+        """
+        self.bot = telebot.TeleBot(token)
         self.audio_processor = AudioProcessor()
-        self.progress_message = None
+        self.progress_emojis = ["⬜"] * 10  # Progress bar representation as emojis
+        self.progress_message = None  # Message instance used for updating the progress bar
 
-    def send_progress(self, chat_id: int, progress: float):
+    def send_progress(self, chat_id, progress):
         """
-        Sends a progress bar to the chat identified by `chat_id` with the percentage of `progress` completed.
-        If a previous progress message exists, it will be edited instead of sending a new message.
+        Updates or sends a new progress message in the chat with the given progress value.
         """
-        progress_text = self._generate_progress_bar(progress)
+        progress_emoji = "🟩"
+        filled_progress = int(progress * 10)
+        progress_bars = self.progress_emojis[:]
+        for i in range(filled_progress):
+            progress_bars[i] = progress_emoji
+        progress_text = "".join(progress_bars)
         if self.progress_message:
+            # If a progress message already exists, edit it
             self.bot.edit_message_text(
                 chat_id=chat_id,
                 message_id=self.progress_message.message_id,
                 text=progress_text
             )
         else:
+            # If no progress message exists, send a new one
             self.progress_message = self.bot.send_message(chat_id, progress_text)
-
-    @staticmethod
-    def _generate_progress_bar(progress: float, total_bars: int = 10) -> str:
-        """
-        Generates a progress bar string with a specified number of `total_bars` and a percentage of `progress`.
-        """
-        progress_emoji = "🟩"
-        empty_bar_emoji = "⬜"
-        filled_bars = int(progress * total_bars)
-        progress_bars = [progress_emoji] * filled_bars + [empty_bar_emoji] * (total_bars - filled_bars)
-        return "".join(progress_bars)
 
     def send_files_via_telegram(self, chat_id, folder):
         """
-        Sends all audio files in the specified `folder` to the chat identified by `chat_id` using Telegram's
-        `send_audio()` method. A progress bar is displayed for each file being sent.
+        Sends all the audio files in the given folder to the specified chat in Telegram.
         """
         for file_name in os.listdir(folder):
             file_path = os.path.join(folder, file_name)
             with open(file_path, 'rb') as audio_file:
-                # Get the size of the file for the progress bar
                 audio_size = os.path.getsize(file_path)
 
-                # Display a progress bar for sending the file
                 with tqdm(total=audio_size, unit='B', unit_scale=True, desc=file_name) as pbar:
                     success = False
                     attempts = 0
                     while not success and attempts < 3:
                         try:
-                            # Send the audio file to the chat
                             self.bot.send_audio(chat_id, audio_file, timeout=600)
                             success = True
                         except telebot.apihelper.ApiException as e:
@@ -71,89 +67,46 @@ class TelegramBot:
                                 print(f"Failed to send {file_name} after {attempts} attempts.")
                                 raise e
                         else:
-                            # Update the progress bar as the file is sent
                             pbar.update(audio_size)
 
-    def process_audio(self, chat_id, url):
+    def start(self):
         """
-        Downloads the audio from the specified `url`, converts it to a compatible format, creates sub-audios from it,
-        removes the original audio file, and sends the resulting files to the chat identified by `chat_id`. Progress
-        bars are displayed for each step of the process.
-        """
-        try:
-            song_title, clip, mp4_file = self.audio_processor.download_audio(url)
-                        # Send progress update after downloading audio
-            self.send_progress(chat_id, 0.2)
-
-            # Convert audio to a compatible format
-            self.audio_processor.convert_audio(song_title, clip, mp4_file)
-            self.send_progress(chat_id, 0.4)
-
-            # Create sub-audios from the main audio file
-            self.audio_processor.create_sub_audios(song_title, clip)
-            self.send_progress(chat_id, 0.8)
-
-            # Remove the original audio file
-            self.audio_processor.remove_audios(song_title, mp4_file)
-
-            # Send resulting audio files to the chat
-            self.send_files_via_telegram(chat_id, song_title)
-        except Exception as e:
-            raise e
-
-    def _register_message_handlers(self):
-        """
-        Registers message handlers for the bot. The '/start' and '/help' commands will trigger the `send_welcome()`
-        function, while all other messages will trigger the `process_message()` function.
+        Starts the Telegram bot, setting up message handlers for different types of messages.
         """
         @self.bot.message_handler(commands=['start', 'help'])
         def send_welcome(message):
-            self.bot.reply_to(message,
-                              "Welcome to the Audio Downloader Bot. Send me a video URL to download the audio.")
+            self.bot.reply_to(message, "Welcome to the Audio Downloader Bot. Send me a video URL to download the audio.")
 
         @self.bot.message_handler(func=lambda message: True)
         def process_message(message):
+            """
+            Processes a message from a chat, downloading and splitting audio from a URL,
+            then sending the audio files to the chat. Updates progress in the chat throughout.
+            """
             chat_id = message.chat.id
             url = message.text
 
             try:
-                # Send initial progress message
                 self.progress_message = None
-                self.send_progress(chat_id, 0.0)
-
-                # Process the audio from the URL
-                self.process_audio(chat_id, url)
-
-                # Send final progress message
+                self.send_progress(chat_id, 0.1)
+                song_title, webm_file = self.audio_processor.download_audio(url)
+                self.send_progress(chat_id, 0.2)
+                duration = self
+                self.audio_processor.get_audio_duration(webm_file)
+                self.send_progress(chat_id, 0.3)
+                self.audio_processor.createSubAudios(duration, song_title, webm_file)
+                self.send_progress(chat_id, 0.6)
+                self.audio_processor.remove_files(webm_file)
+                self.send_progress(chat_id, 0.8)
+                self.send_files_via_telegram(chat_id, song_title)
                 self.send_progress(chat_id, 1.0)
             except Exception as e:
-                if str(e) == "'streamingData'":
-                    # Retry up to 3 times if a 'streamingData' error occurs
-                    max_attempts = 3
-                    attempts = 0
-                    while attempts < max_attempts:
-                        attempts += 1
-                        sleep(5)
-                        try:
-                            self.process_audio(chat_id, url)
-                            break
-                        except Exception as e:
-                            if attempts == max_attempts:
-                                self.bot.send_message(chat_id, f"An error occurred: {e}")
-                else:
-                    # Send error message to chat if an exception occurs
-                    self.bot.send_message(chat_id, f"An error occurred: {e}")
+                # In case of any error during the process, send a message to the chat with the error
+                self.bot.send_message(chat_id, f"An error occurred: {e}")
 
-    def start(self):
-        """
-        Starts the Telegram bot and registers message handlers.
-        """
-        self._register_message_handlers()
-        self.bot.polling()
-
+        self.bot.polling()  # Start polling for messages
 
 if __name__ == '__main__':
-    # Create a new instance of the TelegramBot class and start the bot
-    bot = TelegramBot()
-    bot.start()
-
+    load_dotenv()  # Load environment variables from a .env file
+    bot = TelegramBot(os.environ.get("TELEGRAM_BOT_TOKEN"))  # Create a Telegram bot instance with the token from the environment variables
+    bot.start()  # Start the Telegram bot
